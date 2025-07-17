50e33f04ca6911209879f27ddd1fcbb32a892263
Pablo Menjívar <134906528+pablo-menjivar@users.noreply.github.com>
2025-07-17 03:01:37 -0600

    Images

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ["http://localhost:5174", "http://localhost:5173"],
+  origin: "http://localhost:5173",
   credentials: true
 }))
 // Rutas que NO requieren login
