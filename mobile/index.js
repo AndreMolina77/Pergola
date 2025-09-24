a564f9a38493dc2e1d21097ac32aefd9199d4118
Rafa Vargas <20230402@ricaldone.edu.sv>
2025-09-24 10:59:30 -0600

    HomeScreen completa

--- mobile/index.js
+++ mobile/index.js
@@ -2,6 +2,7 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 import JewelryPreferenceScreen from './src/screen/JewelryPreferenceScreen';
+import HomeScreen from './src/screen/HomeScreen'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
