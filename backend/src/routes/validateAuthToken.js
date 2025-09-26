c065c0d88f04e747467e07b10ab1fd87f41383d8
Pablo Menjívar <134906528+pablo-menjivar@users.noreply.github.com>
2025-09-26 11:20:34 -0600

    Actualizar el rol de usuario de "colaborador" a "empleado" en la validación del token de autenticación

--- backend/src/routes/validateAuthToken.js
+++ backend/src/routes/validateAuthToken.js
@@ -5,7 +5,7 @@ import { validateAuthToken } from "../middlewares/validateAuthToken.js"
 
 const router = express.Router()
 // Ruta para validar el token de autenticación
-router.post("/", validateAuthToken(["admin", "colaborador", "customer"]), (req, res) => {
+router.post("/", validateAuthToken(["admin", "employee", "customer"]), (req, res) => {
   // Si llegamos aqui, significa que el token es válido
   // ESTADO DE OK
   res.status(200).json({ message: "Token válido", valid: true, userType: req.userType || 'unknown', userId: req.userId, email: req.userEmail, name: req.userName, lastName: req.userLastName })
